BenzoRoms/packages_services_Telecomm

[2/6] Breathing missedcall/sms/voicemail
Updated for Marshmallow by Nick

Change-Id: I29594c591156013b90d4c10eb788ab81380ee10b
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/server/telecom/ui/MissedCallNotifierImpl.java
@@ -56,14 +56,21 @@ import android.provider.Settings;
 import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.UserHandle;
+import android.provider.CallLog;
 import android.provider.CallLog.Calls;
+import android.provider.Settings;
 import android.telecom.DefaultDialerManager;
 import android.telecom.PhoneAccount;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.TelephonyManager;
 import android.text.BidiFormatter;
+import android.text.SpannableStringBuilder;
 import android.text.TextDirectionHeuristics;
 import android.text.TextUtils;
+import android.text.format.DateUtils;
+import android.text.style.RelativeSizeSpan;
+
+import java.util.ArrayList;
 
 import com.android.internal.telephony.CallerInfo;
 
@@ -352,18 +359,29 @@ public class MissedCallNotifierImpl extends CallsManagerListenerBase implements 
 
         // Create the notification suitable for display when sensitive information is showing.
         Notification.Builder builder = mNotificationBuilderFactory.getBuilder(contextForUser);
-        builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
+        if (Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.KEY_MISSED_CALL_BREATH, 0) == 1) {
+            builder.setSmallIcon(R.drawable.stat_notify_missed_call_breath)
                 .setColor(mContext.getResources().getColor(R.color.theme_color))
                 .setWhen(callInfo.getCreationTimeMillis())
                 .setContentTitle(mContext.getText(titleResId))
                 .setContentText(expandedText)
                 .setContentIntent(createCallLogPendingIntent(userHandle))
                 .setAutoCancel(true)
+                .setDeleteIntent(createClearMissedCallsPendingIntent(userHandle));
+        } else {
+            builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
+                .setWhen(callInfo.getCreationTimeMillis())
+                .setContentIntent(createCallLogPendingIntent(userHandle))
+                .setAutoCancel(true)
+                .setContentTitle(mContext.getText(titleResId))
+                .setContentText(expandedText)
                 .setDeleteIntent(createClearMissedCallsPendingIntent(userHandle))
                 // Include a public version of the notification to be shown when the missed call
                 // notification is shown on the user's lock screen and they have chosen to hide
                 // sensitive notification information.
                 .setPublicVersion(publicBuilder.build());
+             }
 
         Uri handleUri = callInfo.getHandle();
         String handle = callInfo.getHandleSchemeSpecificPart();

CMRemix/android_packages_services_Telecomm

src/com/android/server/telecom/ui/MissedCallNotifierImpl.java
@@ -61,15 +61,22 @@ import android.provider.Settings;
 import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.UserHandle;
+import android.provider.CallLog;
 import android.provider.CallLog.Calls;
+import android.provider.Settings;
 import android.telecom.DefaultDialerManager;
 import android.telecom.DisconnectCause;
 import android.telecom.PhoneAccount;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.TelephonyManager;
 import android.text.BidiFormatter;
+import android.text.SpannableStringBuilder;
 import android.text.TextDirectionHeuristics;
 import android.text.TextUtils;
+import android.text.format.DateUtils;
+import android.text.style.RelativeSizeSpan;
+
+import java.util.ArrayList;
 
 import com.android.internal.telephony.CallerInfo;
 
@@ -330,18 +337,29 @@ public class MissedCallNotifierImpl extends CallsManagerListenerBase implements 
 
         // Create the notification suitable for display when sensitive information is showing.
         Notification.Builder builder = mNotificationBuilderFactory.getBuilder(contextForUser);
-        builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
+        if (Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.KEY_MISSED_CALL_BREATH, 0) == 1) {
+            builder.setSmallIcon(R.drawable.stat_notify_missed_call_breath)
                 .setColor(mContext.getResources().getColor(R.color.theme_color))
                 .setWhen(call.getCreationTimeMillis())
                 .setContentTitle(mContext.getText(titleResId))
                 .setContentText(expandedText)
                 .setContentIntent(createCallLogPendingIntent(userHandle))
                 .setAutoCancel(true)
+                .setDeleteIntent(createClearMissedCallsPendingIntent(userHandle));
+        } else {
+            builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
+                .setWhen(call.getCreationTimeMillis())
+                .setContentIntent(createCallLogPendingIntent(userHandle))
+                .setAutoCancel(true)
+                .setContentTitle(mContext.getText(titleResId))
+                .setContentText(expandedText)
                 .setDeleteIntent(createClearMissedCallsPendingIntent(userHandle))
                 // Include a public version of the notification to be shown when the missed call
                 // notification is shown on the user's lock screen and they have chosen to hide
                 // sensitive notification information.
                 .setPublicVersion(publicBuilder.build());
+             }
 
         Uri handleUri = call.getHandle();
         String handle = handleUri == null ? null : handleUri.getSchemeSpecificPart();